bb68cc12558325190293e468f3d2927867b86b7e
Pie is Spy <95068281+PieIsSpy@users.noreply.github.com>
2023-08-12 17:47:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var img = document.createElement('button');
 img.setAttribute('src','cute shiorin.jpg');
 
+document.getElementById('giggle01', 'giggle02', 'giggle03', 'giggle04', 'giggle05', 'giggle06', 'giggle07', 'giggle08', 'giggle09', 'giggle10', 'giggle11', 'giggle12', 'giggle13').
+
 const sounds = ['giggle01', 'giggle02', 'giggle03', 'giggle04', 'giggle05', 'giggle06', 'giggle07', 'giggle08', 'giggle09', 'giggle10', 'giggle11', 'giggle12', 'giggle13'];
 
 function randomGiggles() {
